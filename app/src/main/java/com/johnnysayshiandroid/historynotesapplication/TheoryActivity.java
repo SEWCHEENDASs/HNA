94bcae977fdcbc22084b9c11086fd54e047467dc
sew cheenda <cheendasew150397@gmail.com>
2018-01-28 00:41:02 +0800

    UI Images

---
+++ app/src/main/java/com/johnnysayshiandroid/historynotesapplication/TheoryActivity.java
@@ -0,0 +1,13 @@
+package com.johnnysayshiandroid.historynotesapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TheoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theory);
+    }
+}
