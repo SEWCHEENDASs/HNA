14136bcae74d600c386be0dcf3b76032bff72502
sew cheenda <cheendasew150397@gmail.com>
2018-01-27 20:15:14 +0800

    UI Images

--- app/src/main/java/com/johnnysayshiandroid/historynotesapplication/MainActivity.java
+++ app/src/main/java/com/johnnysayshiandroid/historynotesapplication/MainActivity.java
@@ -1,13 +1,29 @@
 package com.johnnysayshiandroid.historynotesapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton Btnchapter1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Btnchapter1=findViewById(R.id.btnChap1);
+
+        Btnchapter1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent activityChangeIntent = new Intent(MainActivity.this, chapter_detailsActivity.class);
+                startActivity(activityChangeIntent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/johnnysayshiandroid/historynotesapplication/chapter_detailsActivity.java
@@ -0,0 +1,13 @@
+package com.johnnysayshiandroid.historynotesapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chapter_detailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter_details);
+    }
+}
