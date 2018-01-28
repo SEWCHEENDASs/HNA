349ed3aeb42bf866537abe5b682a1a129a34e80e
Tong <14049703@myrp.edu.sg>
2018-01-28 09:53:10 +0800

    Done changing the text to white

--- app/src/main/java/com/johnnysayshiandroid/historynotesapplication/SubtopicDetailActivity.java
+++ app/src/main/java/com/johnnysayshiandroid/historynotesapplication/SubtopicDetailActivity.java
@@ -1,12 +1,15 @@
 package com.johnnysayshiandroid.historynotesapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -44,7 +47,23 @@ public class SubtopicDetailActivity extends AppCompatActivity {
             alIDInformation.add(id.getId());
         }
 
-        aa = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, alSubtopic);
+        aa = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, alSubtopic){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                // Get the Item from ListView
+                View view = super.getView(position, convertView, parent);
+
+                // Initialize a TextView for ListView each Item
+                TextView tv = (TextView) view.findViewById(android.R.id.text1);
+
+                // Set the text color of TextView (ListView Item)
+                tv.setTextColor(Color.WHITE);
+
+                // Generate ListView Item using TextView
+                return view;
+            }
+        };
+        
         lvListDetail.setAdapter(aa);
 
         lvListDetail.setOnItemClickListener(new AdapterView.OnItemClickListener() {
